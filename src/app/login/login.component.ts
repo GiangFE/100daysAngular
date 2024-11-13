b4ec4eafd3355e5b89f122da7f6f7d0adf826c06
Giangdh <giangdev27@gmail.com>
2024-11-13 23:09:09 +0700

    feat: add login component

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit } from '@angular/core';
+import { UntypedFormBuilder, UntypedFormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent implements OnInit {
+
+  validateForm!: UntypedFormGroup;
+  passwordVisible = false;
+  password?: string;
+
+  submitForm(): void {
+    if (this.validateForm.valid) {
+      console.log('submit', this.validateForm.value);
+    } else {
+      Object.values(this.validateForm.controls).forEach(control => {
+        if (control.invalid) {
+          control.markAsDirty();
+          control.updateValueAndValidity({ onlySelf: true });
+        }
+      });
+    }
+    this.router.navigate(['/welcome'])
+  }
+
+  constructor(
+    private fb: UntypedFormBuilder,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    this.validateForm = this.fb.group({
+      userName: [null, [Validators.required]],
+      password: [null, [Validators.required]],
+      remember: [true]
+    });
+  }
+
+}
--- src/app/pages/welcome/welcome.module.ts
+++ src/app/pages/welcome/welcome.module.ts
@@ -1,12 +1,29 @@
-import { NgModule } from '@angular/core';
-
-import { WelcomeRoutingModule } from './welcome-routing.module';
-
-import { WelcomeComponent } from './welcome.component';
-
+import { HttpClientModule } from "@angular/common/http";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { BrowserModule } from "@angular/platform-browser";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { NzIconModule } from "ng-zorro-antd/icon";
+import { NzLayoutModule } from "ng-zorro-antd/layout";
+import { NzMenuModule } from "ng-zorro-antd/menu";
+import { AppRoutingModule } from "src/app/app-routing.module";
+import { IconsProviderModule } from "src/app/icons-provider.module";
+import { WelcomeRoutingModule } from "./welcome-routing.module";
+import { WelcomeComponent } from "./welcome.component";
 
 @NgModule({
-  imports: [WelcomeRoutingModule],
+  imports: [
+    FormsModule,
+    HttpClientModule,
+    IconsProviderModule,
+    NzIconModule,
+    NzMenuModule,
+    BrowserModule,
+    BrowserAnimationsModule,
+    NzLayoutModule,
+    AppRoutingModule,
+    WelcomeRoutingModule,
+  ],
   declarations: [WelcomeComponent],
   exports: [WelcomeComponent]
 })
